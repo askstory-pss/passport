855ea4fcbb883dc0540f0603f98726126ab66852
sungsoo <sungsoo@askstory.com>
2024-02-02 10:16:49 +0900

    포트 변경

--- projectName/app.js
+++ projectName/app.js
@@ -44,8 +44,8 @@ db.connect((err) => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(3003, () => {
+  console.log('Server is running on port 3003');
 });
 
 passport.serializeUser((user, done) => {
